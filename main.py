cfdc4b4d3372a3b5b6f049ee8b1673adea0c9b85
semihgecten <semihgecten@gmail.com>
2023-12-20 21:38:07 +0300

    home page tables added

--- main.py
+++ main.py
@@ -16,15 +16,32 @@ cursor = connection.cursor()
 
 # Fetch data (optional)
 # cursor.execute("SELECT * FROM users")
-cursor.execute('SELECT d.driverId, d.driverRef, d.forename, d.surname, d.nationality'
-        ' FROM drivers d'
-        ' ORDER BY driverId')
-rows = cursor.fetchall()
+cursor.execute('select  d.forename, d.surname, r.year , r.name, ds.position, ds.points, bestLap.time '
+            ' from drivers d'
+            ' join driver_standings ds on d.driverId = ds.driverId'
+            ' join races r on ds.raceId = r.raceId'
+            ' join ('
+            ' SELECT l.raceId, l.driverId, MIN(l.time) AS time FROM laptimes l GROUP BY l.raceId, l.driverId'
+            ' ) AS bestLap'
+            ' ON bestLap.raceId = r.raceId AND bestLap.driverId = d.driverId'
+            ' where d.driverId = 15 '
+            ' order by position '
+            ' limit 10')
+# nationality_filter = ''
+# res = cursor.execute(
+#     'SELECT COUNT(*) FROM drivers WHERE 1=1 AND (CASE WHEN "" = ?  THEN 1=1 ELSE nationality=? END)',
+#     (nationality_filter, nationality_filter)).fetchone()[0]
 
+# print(res)
+
+
+rows = cursor.fetchall()
 for row in rows:
     for x in row:
         print(str(x), end=" | ")
     print("")
 
 # Close the connection
+
+
 connection.close()
